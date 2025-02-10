b674c7d9f9a0b4b302a2634c56f630f2077b14fa
Alexa Yudhadinatha <alexa.zeta@student.umn.ac.id>
2025-02-11 02:13:39 +0700

    Step 10

--- app/src/main/java/ac/id/umn/basicstatecodelab/WellnessScreen.kt
+++ app/src/main/java/ac/id/umn/basicstatecodelab/WellnessScreen.kt
@@ -1,8 +1,13 @@
 import ac.id.umn.basicstatecodelab.StatefulCounter
+import ac.id.umn.basicstatecodelab.WellnessTasksList
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
---
+++ app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTask.kt
@@ -0,0 +1,3 @@
+package ac.id.umn.basicstatecodelab
+
+data class WellnessTask(val id: Int, val label: String)
