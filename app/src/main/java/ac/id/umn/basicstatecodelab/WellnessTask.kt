34820a81ad6b748818f4287263404c6dd9076bcf
Alexa Yudhadinatha <alexa.zeta@student.umn.ac.id>
2025-02-11 02:35:46 +0700

    Step 12

--- app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTask.kt
+++ app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTask.kt
@@ -1,3 +1,15 @@
 package ac.id.umn.basicstatecodelab
 
-data class WellnessTask(val id: Int, val label: String)
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+
+//data class WellnessTask(val id: Int, val label: String, var checked: Boolean = false)
+
+class WellnessTask(
+    val id: Int,
+    val label: String,
+    initialChecked: Boolean = false
+) {
+    var checked by mutableStateOf(initialChecked)
+}
--- app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTaskItem.kt
+++ app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTaskItem.kt
@@ -1,23 +1,46 @@
 package ac.id.umn.basicstatecodelab
 
+import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WellnessTaskItem(
-    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+    taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    var checkedState by rememberSaveable { mutableStateOf(false) }
-
-    WellnessTaskItem(
-        taskName = taskName,
-        checked = checkedState,
-        onCheckedChange = { newValue -> checkedState = newValue },
-        onClose = onClose,
-        modifier = modifier,
-    )
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
+            text = taskName
+        )
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
 }
