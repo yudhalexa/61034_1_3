04528e6d0093db912a68c50eeb34c115906767be
Alexa Yudhadinatha <alexa.zeta@student.umn.ac.id>
2025-02-11 02:17:04 +0700

    Step 11

---
+++ app/src/main/java/ac/id/umn/basicstatecodelab/WellnessTaskItem.kt
@@ -0,0 +1,23 @@
+package ac.id.umn.basicstatecodelab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = onClose,
+        modifier = modifier,
+    )
+}
